8f5c975762aca88dc971f7c9df9098557a8067b8
frstrtr <frstrtr@gmail.com>
2025-07-08 12:25:01 +0400

    update boat 14

--- src/menu_config.py
+++ src/menu_config.py
@@ -30,7 +30,7 @@ DEFAULT_MENU_STRUCTURE = {
             [{"text": "Yamaha 1 Red", "target": "boat5"}, {"text": "Yamaha 2 Blue", "target": "boat9"}],
             [{"text": "Mercury 17 Ft", "target": "boat3"}, {"text": "Catamaran 40 Ft", "target": "boat4"}],
             [{"text": "LIMITLESS fishing", "target": "boat6"}, {"text": "Scarab 255 LX Wake", "target": "boat10"}],
-            [{"text": "Fishing Expeditions", "target": "boat13"}],
+            [{"text": "Fishing Expeditions", "target": "boat13"}, {"text": "Sport Fisher 40 Ft", "target": "boat14"}],
             [{"text": "⬅️ Назад", "target": "main_menu"}]
         ]
     },
@@ -60,6 +60,7 @@ DEFAULT_MENU_STRUCTURE = {
     "boat11":      {"type": "content", "text_path": "boat11.html", "back_to": "boats_menu"},
     "boat12":      {"type": "content", "text_path": "boat12.html", "back_to": "boats_menu"},
     "boat13":      {"type": "content", "text_path": "boat13.html", "back_to": "boats_menu"},
+    "boat14":      {"type": "content", "text_path": "boat14.html", "back_to": "boats_menu"},
 }
 
 # --- Client-Specific Menu Assignments ---
